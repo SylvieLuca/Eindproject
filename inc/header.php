d3a01e3ed1352726510bb1ae6f2b7c3f5567b46d
Sylvie Luca <sylvieluca@hotmail.com>
2021-05-16 22:35:48 +0200

    New Logo

--- inc/header.php
+++ inc/header.php
@@ -12,3 +12,26 @@
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
   </head>
+  <body>
+  <header>
+    <nav class="nav collapsible">
+      <a class="nav__brand" href="index.html"><img class="link__logo" src="images/logoSmall.png" alt="Quetie and Smoetie's Quality Scratching Logo" /></a>
+      <svg class="icon icon--white nav__toggler">
+        <use xlink:href="images/sprite.svg#menu"></use>
+      </svg>
+      <ul class="list nav__list collapsible__content">
+        <?php
+        if (isset($_SESSION["userid"])) {
+          echo "<li class='nav__item'><a href='shop.html'>Shop</a></li>";
+          echo "<li class='nav__item'><a href='profile.php'>Profile Page</a></li>";
+          echo "<li class='nav__item'><a href='includes/logout.inc.php'>Log Out</a></li>";
+        }
+        else {
+          echo "<li class='nav__item'><a href='signup.php'>Sign Up</a></li>";
+          echo "<li class='nav__item'><a href='login.php'>Log In</a></li>";
+        }
+      ?>
+      </ul>
+    </nav>
+  </header>
+  </body>
