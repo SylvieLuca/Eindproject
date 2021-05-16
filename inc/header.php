593cf3e7d618f60dfa7f4dc18003d74c0319332f
Sylvie Luca <sylvieluca@hotmail.com>
2021-05-16 02:31:09 +0200

    Combining everything - First attempt

--- inc/header.php
+++ inc/header.php
@@ -5,12 +5,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quetie and Smoetie's Quality Scratching</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
   </head>
-  <body>
+<!--   <body>
     <nav>
       <div>
         <ul>
@@ -30,4 +32,4 @@
           </ul>
       </div>
     </nav>
-          </body>
+  </body> -->
