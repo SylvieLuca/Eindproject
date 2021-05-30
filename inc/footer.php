3a3aaa4083644c281d7d1db027d345e0b7e52d44
Sylvie Luca <sylvieluca@hotmail.com>
2021-05-30 04:42:12 +0200

    Social Media Icons Google Search Links

---
+++ inc/footer.php
@@ -0,0 +1,78 @@
+<footer class="block block--dark footer">
+  <div class="grid footer__sections">
+
+    <section class="collapsible footer__section">
+      <header class="collapsible__header">
+        <h2 class="collapsible__heading footer__heading">Company</h2>
+        <svg class="icon icon--white collapsible__chevron">
+          <use xlink:href="images/sprite.svg#chevron"></use>
+        </svg>
+      </header>
+      <div class="collapsible__content">
+        <ul class="list">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Affiliates</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="collapsible collapsible--expanded footer__section">
+      <header class="collapsible__header">
+        <h2 class="collapsible__heading footer__heading">Products</h2>
+        <svg class="icon icon--white collapsible__chevron">
+          <use xlink:href="images/sprite.svg#chevron"></use>
+        </svg>
+      </header>
+      <div class="collapsible__content">
+        <ul class="list">
+          <li><a href="#">Website Hosting</a></li>
+          <li><a href="#">Free Automated Wordpress</a></li>
+          <li><a href="#">Migrations</a></li>
+        </ul>
+      </div>
+    </section>
+
+
+
+    <section class="collapsible footer__section">
+      <header class="collapsible__header">
+        <h2 class="collapsible__heading footer__heading">Support</h2>
+        <svg class="icon icon--white collapsible__chevron">
+          <use xlink:href="images/sprite.svg#chevron"></use>
+        </svg>
+      </header>
+      <div class="collapsible__content">
+        <ul class="list">
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="#">Knowledge Base</a></li>
+          <li><a href="#">FAQ</a></li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="collapsible footer__section">
+      <div class="socialMedia">
+        <a class="bg-icons8-facebook-50" target="_blank" href="http://www.google.com/search?q=Quetie+and+Smoetie+Facebook"></a>
+        <a class="bg-icons8-instagram-50" target="_blank" href="http://www.google.com/search?q=Quetie+and+Smoetie+Instagram"></a>
+        <a class="bg-icons8-reddit-50" target="_blank" href="http://www.google.com/search?q=Quetie+and+Smoetie+Reddit"></a>
+        <a class="bg-icons8-twitter-50" target="_blank" href="http://www.google.com/search?q=Quetie+and+Smoetie+Twitter"></a>
+      </div>
+    </section>
+
+    <section class="footer__brand">
+      <img
+        src="images/logoSmall.png"
+        alt="Quetie and Smoetie's Quality Scratching Logo"
+      />
+      <p class="footer__copyright">Copyright <?php echo date("Y"); ?> - Sylvie Luca</p>
+    </section>
+
+  </div>
+  
+  <div class="privacyAndTerms">
+    <a href="https://www.freeprivacypolicy.com/live/d19021ec-2542-43c2-899f-a7942c59718d">Privacy Policy</a>
+    <a href="https://www.freeprivacypolicy.com/live/cd687b27-9b46-4c3e-8ba9-69d6aef2cc0d">Terms & Conditions</a>
+    </div>
+
+</footer>
